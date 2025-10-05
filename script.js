49630e737898b1b1f66c406f7f1a8968ee9e0452
mirafl0res <wasp.answer5v@icloud.com>
2025-10-05 17:29:44 +0200

    set up API_ROOT, API_KEY and create fetchAlbums()

---
+++ script.js
@@ -0,0 +1,36 @@
+// const API_KEY = "ENTER_API_KEY"
+const API_ROOT = "https://ws.audioscrobbler.com/2.0/";
+
+const fetchAlbums = async (artistName) => {
+  const encodedArtist = encodeURIComponent(artistName);
+  const url = `${API_ROOT}?method=artist.gettopalbums&artist=${encodedArtist}&autocorrect=1&api_key=${API_KEY}&format=json`;
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent":
+          "Good Vibrations/1.0 (https://github.com/mirafl0res/interactive-web-assignment.git)",
+      },
+    });
+
+    const parsedData = await response.json();
+    const topAlbums = parsedData.topalbums.album;
+    console.log(topAlbums);
+
+    const albumContainer = document.createElement("div");
+    topAlbums.forEach((album) => {
+      const albumName = document.createElement("p");
+      albumName.textContent = album.name;
+      albumContainer.appendChild(albumName);
+    });
+
+    const container = document.getElementsByClassName("container")[0];
+    container.appendChild(albumContainer);
+
+  } catch (error) {
+    console.error("Error fetching albums:", error);
+  }
+};
+
+console.log("hello");
+fetchAlbums("The Beatles");
